79e2b09d36a0a2e9de049c43003e88f5a358eaed
Reherc <amaury.vanoorenberghe@yahoo.com>
2021-07-21 19:33:19 +0200

    Mod ported from common to single repository

---
+++ Distance.Anolytics/Harmony/Assembly-CSharp/Analytics/LogEvent.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace Distance.Anolytics.Harmony
+{
+	[HarmonyPatch(typeof(Analytics), "LogEvent")]
+	internal static class Analytics__LogEvent
+	{
+		[HarmonyPrefix]
+		internal static bool Prefix(string page, string category, string action, string opt_label, int opt_value)
+		{
+			Mod mod = Mod.Instance;
+
+			if (mod.Config.LogAnalytics)
+			{
+				mod.Logger.Info($"Analytics request:\n  Page: {page}\n  Category: {category}\n  Action: {action}\n  Opt. Label: {opt_label}\n  Opt. Value: {opt_value}");
+			}
+
+			return !mod.Config.ShutDownAnalytics;
+		}
+	}
+}
---
+++ Distance.Anolytics/Harmony/Assembly-CSharp/FileEx/WriteAllText.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System;
+
+namespace Distance.Anolytics.Harmony
+{
+	[HarmonyPatch(typeof(FileEx), "WriteAllText")]
+	internal static class FileEx__WriteAllText
+	{
+		[HarmonyPrefix]
+		internal static bool Prefix(string path)
+		{
+			return !string.Equals(path, "playtesting_data.txt", StringComparison.OrdinalIgnoreCase);
+		}
+	}
+}
